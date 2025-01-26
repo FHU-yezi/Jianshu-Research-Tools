259d7e366febc68bc418993b96d5632e2cf10e13
FHU-yezi <yehaowei20060411@qq.com>
2025-01-26 11:07:22 +0800

    feat: 优化对象 `__repr__` 表示

--- jkit/_base.py
+++ jkit/_base.py
@@ -43,7 +43,8 @@ class DataObject(Struct, frozen=True, eq=True, kw_only=True):
 
 
 class ResourceObject:
-    pass
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}()"
 
 
 class CheckableMixin(metaclass=ABCMeta):
