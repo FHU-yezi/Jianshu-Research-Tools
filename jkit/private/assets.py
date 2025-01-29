a87df999714896cc976ac3698740612ce28f642d
FHU-yezi <yehaowei20060411@qq.com>
2025-01-29 23:59:55 +0800

    refactor: 重新设计资产私有模块

--- jkit/private/assets.py
+++ jkit/private/assets.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from collections.abc import AsyncGenerator
 from contextlib import suppress
-from datetime import datetime
 from decimal import Decimal
+from typing import Literal
 
 from httpx import HTTPStatusError
 from msgspec import DecodeError
@@ -11,7 +11,6 @@ from msgspec import DecodeError
 from jkit._base import DataObject, ResourceObject
 from jkit._network import JSON_DECODER, send_request
 from jkit._normalization import (
-    normalize_assets_amount,
     normalize_assets_amount_precise,
     normalize_datetime,
     normalize_percentage,
@@ -20,6 +19,7 @@ from jkit.constants import _ASSETS_ACTION_FAILED_STATUS_CODE
 from jkit.constraints import (
     NonEmptyStr,
     NonNegativeFloat,
+    NonNegativeInt,
     NormalizedDatetime,
     Percentage,
     PositiveInt,
@@ -27,17 +27,19 @@ from jkit.constraints import (
 from jkit.credential import JianshuCredential
 from jkit.exceptions import BalanceNotEnoughError, WeeklyConvertLimitExceededError
 
+AssetsTransactionType = Literal["INCOME", "EXPENSE"]
+BenefitCardType = Literal["PENDING", "ACTIVE", "EXPIRED"]
 
-class AssetsTransactionRecord(DataObject, frozen=True):
+
+class AssetsTransactionData(DataObject, frozen=True):
     id: PositiveInt
     time: NormalizedDatetime
-    type_id: PositiveInt
-    type_text: NonEmptyStr
-    amount: float
-    amount_precise: Decimal
+    type: AssetsTransactionType
+    category: NonEmptyStr
+    amount: Decimal
 
 
-class FPRewardsRecord(DataObject, frozen=True):
+class FpHoldingRewardData(DataObject, frozen=True):
     time: NormalizedDatetime
     own_amount: Decimal
     level1_referral_amount: Decimal
@@ -45,129 +47,80 @@ class FPRewardsRecord(DataObject, frozen=True):
     total_amount: Decimal
 
 
-class BenefitCardsInfo(DataObject, frozen=True):
-    total_amount: NonNegativeFloat
+class BenefitCardsInfoData(DataObject, frozen=True):
+    total_amount: NonNegativeInt
     estimated_benefits_percent: Percentage
 
 
-class UnusedBenfitCardRecord(DataObject, frozen=True):
-    amount: NonNegativeFloat
+class BenefitCardData(DataObject, frozen=True):
+    type: BenefitCardType
+    amount: NonNegativeInt
     start_time: NormalizedDatetime
     end_time: NormalizedDatetime
-
-    @property
-    def is_valid(self) -> bool:
-        return self.start_time <= datetime.now() <= self.end_time
-
-
-class ActiveBenfitCardRecord(DataObject, frozen=True):
-    amount: NonNegativeFloat
-    start_time: NormalizedDatetime
-    end_time: NormalizedDatetime
-    estimated_benefits_precent: NonNegativeFloat
-
-    @property
-    def is_valid(self) -> bool:
-        return self.start_time <= datetime.now() <= self.end_time
-
-
-class ExpiredBenfitCardRecord(DataObject, frozen=True):
-    amount: NonNegativeFloat
-    start_time: NormalizedDatetime
-    end_time: NormalizedDatetime
-    benefits: float
-    benfits_precise: Decimal
-
-    @property
-    def is_valid(self) -> bool:
-        return self.start_time <= datetime.now() <= self.end_time
+    estimated_benefits_precent: NonNegativeFloat | None
 
 
 class Assets(ResourceObject):
     def __init__(self, *, credential: JianshuCredential) -> None:
         self._credential = credential
 
-    async def iter_fp_records(
-        self, *, max_id: int | None = None
-    ) -> AsyncGenerator[AssetsTransactionRecord, None]:
-        now_max_id = max_id
+    async def iter_assets_transactions(
+        self,
+        *,
+        type: Literal["FP", "FTN"],
+        min_id: int = 0,
+        max_id: int | None = None,
+    ) -> AsyncGenerator[AssetsTransactionData, None]:
+        current_max_id = max_id
 
         while True:
             data = await send_request(
                 datasource="JIANSHU",
                 method="GET",
-                path="/asimov/fp_wallets/transactions",
-                params={"since_id": 0, "max_id": now_max_id}
-                if now_max_id
-                else {"since_id": 0},
+                path="/asimov/fp_wallets/transactions"
+                if type == "FP"
+                else "/asimov/fp_wallets/jsb_transactions",
+                params={
+                    "since_id": min_id,
+                    "max_id": current_max_id,
+                }
+                if current_max_id
+                else {"since_id": min_id},
                 cookies=self._credential.cookies,
                 response_type="JSON",
             )
             if not data["transactions"]:
                 return
 
-            now_max_id = data["transactions"][-1]["id"]
+            current_max_id = data["transactions"][-1]["id"]
 
             for item in data["transactions"]:
-                is_out = item["io_type"] == 2  # noqa: PLR2004
-                yield AssetsTransactionRecord(
+                # 1：收入 2：支出
+                transaction_type: AssetsTransactionType = (
+                    "INCOME" if item["io_type"] == 1 else "EXPENSE"
+                )
+                yield AssetsTransactionData(
                     id=item["id"],
                     time=normalize_datetime(item["time"]),
-                    type_id=item["tn_type"],
-                    type_text=item["display_name"],
-                    amount=normalize_assets_amount(item["amount"])
-                    * (-1 if is_out else 1),
-                    amount_precise=normalize_assets_amount_precise(
-                        item["amount_18"] * (-1 if is_out else 1)
+                    type=transaction_type,
+                    category=item["display_name"],
+                    amount=normalize_assets_amount_precise(
+                        # 将获得与消耗数值统一为正
+                        abs(item["amount_18"])
                     ),
                 )._validate()
 
-    async def iter_ftn_records(
-        self, *, max_id: int | None = None
-    ) -> AsyncGenerator[AssetsTransactionRecord, None]:
-        now_max_id = max_id
-
-        while True:
-            data = await send_request(
-                datasource="JIANSHU",
-                method="GET",
-                path="/asimov/fp_wallets/jsb_transactions",
-                params={"since_id": 0, "max_id": now_max_id}
-                if now_max_id
-                else {"since_id": 0},
-                cookies=self._credential.cookies,
-                response_type="JSON",
-            )
-            if not data["transactions"]:
-                return
-
-            now_max_id = data["transactions"][-1]["id"]
-
-            for item in data["transactions"]:
-                is_out = item["io_type"] == 2  # noqa: PLR2004
-                yield AssetsTransactionRecord(
-                    id=item["id"],
-                    time=normalize_datetime(item["time"]),
-                    type_id=item["tn_type"],
-                    type_text=item["display_name"],
-                    amount=normalize_assets_amount(item["amount"])
-                    * (-1 if is_out else 1),
-                    amount_precise=normalize_assets_amount_precise(
-                        item["amount_18"] * (-1 if is_out else 1)
-                    ),
-                )._validate()
-
-    async def iter_fp_rewards_records(
-        self, *, page_size: int = 10
-    ) -> AsyncGenerator[FPRewardsRecord, None]:
-        now_page = 1
+    async def iter_fp_holding_rewards(
+        self,
+    ) -> AsyncGenerator[FpHoldingRewardData, None]:
+        current_page = 1
 
         while True:
             data = await send_request(
                 datasource="JIANSHU",
                 method="GET",
                 path="/asimov/fp_wallets/jsd_rewards",
-                params={"page": now_page, "count": page_size},
+                params={"page": current_page, "count": 20},
                 cookies=self._credential.cookies,
                 response_type="JSON",
             )
@@ -175,7 +128,7 @@ class Assets(ResourceObject):
                 return
 
             for item in data["transactions"]:
-                yield FPRewardsRecord(
+                yield FpHoldingRewardData(
                     time=normalize_datetime(item["time"]),
                     own_amount=normalize_assets_amount_precise(item["own_reards18"]),
                     level1_referral_amount=normalize_assets_amount_precise(
@@ -189,10 +142,10 @@ class Assets(ResourceObject):
                     ),
                 )._validate()
 
-            now_page += 1
+            current_page += 1
 
     @property
-    async def benefit_cards_info(self) -> BenefitCardsInfo:
+    async def benefit_cards_info(self) -> BenefitCardsInfoData:
         data = await send_request(
             datasource="JIANSHU",
             method="GET",
@@ -201,24 +154,28 @@ class Assets(ResourceObject):
             response_type="JSON",
         )
 
-        return BenefitCardsInfo(
-            total_amount=float(normalize_assets_amount_precise(data["total_amount18"])),
+        return BenefitCardsInfoData(
+            total_amount=int(normalize_assets_amount_precise(data["total_amount18"])),
             estimated_benefits_percent=normalize_percentage(
                 data["total_estimated_benefits"]
             ),
         )._validate()
 
-    async def iter_unused_benefit_cards(
-        self, *, page_count: int = 10
-    ) -> AsyncGenerator[UnusedBenfitCardRecord, None]:
+    async def iter_benefit_cards(
+        self, *, type: BenefitCardType
+    ) -> AsyncGenerator[BenefitCardData, None]:
         now_page = 1
 
         while True:
             data = await send_request(
                 datasource="JIANSHU",
                 method="GET",
-                path="/asimov/fp_wallets/benefit_cards/unsent",
-                params={"page": now_page, "count": page_count},
+                path={
+                    "PENDING": "/asimov/fp_wallets/benefit_cards/unsent",
+                    "ACTIVE": "/asimov/fp_wallets/benefit_cards/active",
+                    "EXPIRED": "/asimov/fp_wallets/benefit_cards/expire",
+                }[type],
+                params={"page": now_page, "count": 20},
                 cookies=self._credential.cookies,
                 response_type="JSON",
             )
@@ -227,84 +184,31 @@ class Assets(ResourceObject):
                 return
 
             for item in data["benefit_cards"]:
-                yield UnusedBenfitCardRecord(
-                    amount=float(normalize_assets_amount_precise(item["amount18"])),
+                yield BenefitCardData(
+                    type=type,
+                    amount=int(normalize_assets_amount_precise(item["amount18"])),
                     start_time=normalize_datetime(item["start_time"]),
                     end_time=normalize_datetime(item["end_time"]),
+                    estimated_benefits_precent=item["estimated_benefits"] / 100
+                    if "estimated_benefits" in item
+                    else None,
                 )._validate()
 
             now_page += 1
 
-    async def iter_active_benefit_cards(
-        self, *, page_count: int = 10
-    ) -> AsyncGenerator[ActiveBenfitCardRecord, None]:
-        now_page = 1
-
-        while True:
-            data = await send_request(
-                datasource="JIANSHU",
-                method="GET",
-                path="/asimov/fp_wallets/benefit_cards/active",
-                params={"page": now_page, "count": page_count},
-                cookies=self._credential.cookies,
-                response_type="JSON",
-            )
-
-            if not data["benefit_cards"]:
-                return
-
-            for item in data["benefit_cards"]:
-                yield ActiveBenfitCardRecord(
-                    amount=float(normalize_assets_amount_precise(item["amount18"])),
-                    start_time=normalize_datetime(item["start_time"]),
-                    end_time=normalize_datetime(item["end_time"]),
-                    estimated_benefits_precent=item["estimated_benefits"] / 100,
-                )._validate()
-
-            now_page += 1
-
-    async def iter_expired_benefit_cards(
-        self, *, page_count: int = 10
-    ) -> AsyncGenerator[ExpiredBenfitCardRecord, None]:
-        now_page = 1
-
-        while True:
-            data = await send_request(
-                datasource="JIANSHU",
-                method="GET",
-                path="/asimov/fp_wallets/benefit_cards/expire",
-                params={"page": now_page, "count": page_count},
-                cookies=self._credential.cookies,
-                response_type="JSON",
-            )
-
-            if not data["benefit_cards"]:
-                return
-
-            for item in data["benefit_cards"]:
-                yield ExpiredBenfitCardRecord(
-                    amount=float(normalize_assets_amount_precise(item["amount18"])),
-                    start_time=normalize_datetime(item["start_time"]),
-                    end_time=normalize_datetime(item["end_time"]),
-                    benefits=round(
-                        float(normalize_assets_amount_precise(item["benefits"])), 2
-                    ),
-                    benfits_precise=normalize_assets_amount_precise(item["benefits"]),
-                )._validate()
-
-            now_page += 1
-
-    async def fp_to_ftn(self, /, amount: int | float) -> None:
+    # FIXME: 转换数量为 0
+    async def fp_to_ftn(self, amount: int | float, /) -> None:
         if amount <= 0:
             raise ValueError("转换的简书钻数量必须大于 0")
 
         try:
-            with suppress(DecodeError):  # TODO
+            # TODO: send_request 函数支持 response_type=None
+            with suppress(DecodeError):
                 await send_request(
                     datasource="JIANSHU",
                     method="POST",
                     path="/asimov/fp_wallets/exchange_jsb",
-                    body={"count": str(amount)},
+                    body={"count": amount},
                     cookies=self._credential.cookies,
                     response_type="JSON",
                 )
@@ -321,17 +225,19 @@ class Assets(ResourceObject):
 
             raise e from None
 
-    async def ftn_to_fp(self, /, amount: int | float) -> None:
+    # FIXME: 转换数量为 0
+    async def ftn_to_fp(self, amount: int | float, /) -> None:
         if amount <= 0:
             raise ValueError("转换的简书贝数量必须大于 0")
 
         try:
-            with suppress(DecodeError):  # TODO
+            # TODO: send_request 函数支持 response_type=None
+            with suppress(DecodeError):
                 await send_request(
                     datasource="JIANSHU",
                     method="POST",
                     path="/asimov/fp_wallets/exchange_jsd",
-                    body={"count": str(amount)},
+                    body={"count": amount},
                     cookies=self._credential.cookies,
                     response_type="JSON",
                 )
@@ -345,5 +251,4 @@ class Assets(ResourceObject):
                     raise WeeklyConvertLimitExceededError(
                         "超出每周转换额度限制"
                     ) from None
-
             raise e from None
